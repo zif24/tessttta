70b01661fdec06dd3ad240373a4b256628e8340a
zif24 <46397782+zif24@users.noreply.github.com>
2024-02-13 15:39:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <cassert>
 
+int Sum(int a, int b)
+{
+  return a+b;
+}
+
 void RightAnswer()
 { 
   std::cout<< "RightAnswer";
   assert(Sum(5, 5) == 10);   
 }
-int Sum(int a, int b)
-{
-  return a+b;
-}
+
 int main()
 {
   
