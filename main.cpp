492c3b1378f7be4fdfa070116783329353778296
zif24 <46397782+zif24@users.noreply.github.com>
2024-02-13 15:38:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <cassert>
+
+void RightAnswer()
+{ 
+  std::cout<< "RightAnswer";
+  assert(Sum(5, 5) == 10);   
+}
 int Sum(int a, int b)
 {
   return a+b;
 }
 int main()
 {
-  assert(Sum(5, 5) == 5);
+  
   std::cout<< "Hello World!";
   return 0;
 }
